0caf210b2d4d2a8fb934274a7387f6dce61b3211
Only Jonesy <andy@onlyjonesy.com.au>
2021-12-06 12:45:50 +1100

    Add only jonesy to the footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()} Only Jonesy, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
